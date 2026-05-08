74023c46b4a635a2fc5f26993c4feb1cb0254d05
Cuong Liam <cuongdeptrai2706@gmail.com>
2026-05-08 10:10:09 +0700

    fin

--- src/main/java/com/example/model/Book.java
+++ src/main/java/com/example/model/Book.java
@@ -1,12 +1,24 @@
 package com.example.model;
 
 
+import jakarta.validation.constraints.*;
+
 public class Book {
 
     private Long id;
+
+    @NotBlank(message = "Ko được để trống")
+    @Size(min = 3, max = 100, message = "tên từ 3-100 kí tự")
     private String title;
+
+    @NotBlank(message = "ko được để trống")
     private String author;
+
+    @Max(value = 2, message = "số lượng phải >= 0")
+    @NotBlank(message = "ko được để trống")
     private Integer quantity;
+
+    @NotBlank(message = "ko được để trống")
     private String coverImage;
 
     public Book() {
